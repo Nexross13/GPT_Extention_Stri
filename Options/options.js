e1fa48723a98e40e509fba8d3c2e626dcc5388a9
Enzo Hurtado <hurtado.enzo1@gmail.com>
2023-09-20 17:01:20 +0200

    Publication de la V1 (Complète)

---
+++ Options/options.js
@@ -0,0 +1,54 @@
+// Charger les options
+function loadOptions() {
+  chrome.storage.sync.get(['theme', 'model', 'prefix', 'apiKey'], function(items) {
+    document.getElementById('theme').value = items.theme || 'light';
+    document.getElementById('model').value = items.model || 'gpt-3.5-turbo';
+    document.getElementById('prefix').value = items.prefix || 'Exemple : Vous êtes un expert en informatique, répondez-moi avec seulement la ou les réponses à la question : ';
+    document.getElementById('apiKey').value = items.apiKey || '';
+
+    // Appliquer le thème initial
+    applyTheme(items.theme || 'light');
+  });
+}
+
+// Sauvegarder les options
+function saveOptions() {
+  const theme = document.getElementById('theme').value;
+  const model = document.getElementById('model').value;
+  const prefix = document.getElementById('prefix').value;
+  const apiKey = document.getElementById('apiKey').value;
+
+  chrome.storage.sync.set({
+    theme: theme,
+    model: model,
+    prefix: prefix,
+    apiKey: apiKey,
+  }, function() {
+    // Mettre à jour le statut pour informer l'utilisateur que les options ont été sauvegardées
+    const status = document.getElementById('status');
+    status.textContent = 'Options sauvegardées.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 1500);
+  });
+
+  // Appliquer le nouveau thème
+  applyTheme(theme);
+}
+
+function applyTheme(theme) {
+  const bodyElement = document.getElementById('mainBody');
+  console.log("Applying theme: ", theme);  // Pour le débogage
+  if (theme === 'dark') {
+    bodyElement.classList.add('dark-theme');
+  } else {
+    bodyElement.classList.remove('dark-theme');
+  }
+}
+
+
+// Lorsque le DOM est entièrement chargé
+document.addEventListener('DOMContentLoaded', loadOptions);
+
+// Attacher l'événement de sauvegar
+document.getElementById('save').addEventListener('click', saveOptions);
---
+++ background.js
@@ -0,0 +1,92 @@
+// Fonction pour copier du texte dans le presse-papier
+function copyTextToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+      console.log('Text successfully copied');
+    }).catch(error => {  // <-- Ici, utilisez "error" au lieu de "err"
+      let errorMessage = error.message === 'Invalid Copy' ? 'Impossible de mettre la réponse dans le presse papier' : 'Une erreur s\'est produite lors de la mise dans le presse papier';
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: 'Erreur de presse papier',
+        message: errorMessage
+      });
+    });
+}
+
+  
+  // Initialisation du menu contextuel
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: "chatGPT",
+      title: "Répondre",
+      contexts: ["selection"]
+    });
+  });
+  
+  // Écouteur pour le menu contextuel
+  chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    chrome.storage.sync.get(['modelChoice', 'prefix', 'apiKey'], function (items) {
+      const model = items.modelChoice || 'gpt-4-0613';
+      const prefix = items.prefix || 'Votre préfixe par défaut';
+      const apiKey = items.apiKey || 'Votre clé API par défaut';
+  
+      const selectedText = info.selectionText;
+      const modifiedSelectedText = prefix + " " + selectedText;
+  
+      fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${apiKey}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          model: model,
+          messages: [{
+            role: "user",
+            content: modifiedSelectedText
+          }]
+        })
+      })
+      .then(response => {
+        if (response.status !== 200) {
+          throw new Error('Invalid API Key');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (apiKey === 'Votre clé API par défaut') {
+          chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: 'Erreur',
+            message: 'Clé API manquante. Veuillez ajouter une clé API.'
+          });
+        } else if (data && data.choices && data.choices[0] && data.choices[0].message) {
+          const assistantMessage = data.choices[0].message.content;
+          chrome.notifications.create({
+            type: 'basic',
+            iconUrl: 'icon.png',
+            title: 'Réponse de l\'assistant',
+            message: assistantMessage
+          });
+  
+          chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            function: copyTextToClipboard,
+            args: [assistantMessage]
+          });
+        }
+      })
+      .catch(error => {
+        //console.error('Erreur lors de l\'appel à GPT:', error);
+        let errorMessage = error.message === 'Invalid API Key' ? 'Clé API fausse' : 'Une erreur s\'est produite lors de l\'appel à l\'API.';
+        chrome.notifications.create({
+          type: 'basic',
+          iconUrl: 'icon.png',
+          title: 'Erreur API',
+          message: errorMessage
+        });
+      });
+    });
+  });
+  
